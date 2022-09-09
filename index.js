6a3afb88f72abcbf86d743bce5451e9237798042
Habon <habonhassan01@gmail.com>
2022-09-08 20:54:16 -0400

    Add logic to determine winner

--- index.js
+++ index.js
@@ -4,6 +4,23 @@ let randomNumber1 = Math.floor(Math.random() * 6) + 1;
 
 let randomDiceImage = `images/dice${randomNumber1}.png`; // path of dice images
 
-let getImage = document.querySelectorAll("img")[0] // get image element with index 0 (i.e. first image)
+let getImage1 = document.querySelectorAll("img")[0].setAttribute("src", randomDiceImage) // get and set image with new src
 
-let setImage = getImage.setAttribute("src", randomDiceImage) // change sourceo of image
+
+// Dice 2
+let randomNumber2 = Math.floor(Math.random() * 6) + 1; 
+
+let randomDiceImage2 = `images/dice${randomNumber2}.png`; // path of dice images
+
+let getImage2 = document.querySelectorAll("img")[1].setAttribute("src", randomDiceImage2) // get and set image with new src
+
+// If player 1 has a higher number, player 1 wins
+
+let setText = document.querySelector("h1").innerHTML
+if (randomNumber1 > randomNumber2){
+  document.querySelector("h1").innerHTML = "✨ Player 1 Wins! ✨ "
+} else if (randomNumber1 < randomNumber2){
+  document.querySelector("h1").innerHTML = "✨ Player 2 Wins! ✨ "
+} else {
+  document.querySelector("h1").innerHTML = "🚩 Draw"
+}
