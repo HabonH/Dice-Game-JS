d4357f4eb45a10c908cf252a8575cf96cd74cc59
Habon <habonhassan01@gmail.com>
2022-09-08 22:13:42 -0400

    Add styling

--- index.js
+++ index.js
@@ -18,9 +18,9 @@ let getImage2 = document.querySelectorAll("img")[1].setAttribute("src", randomDi
 
 let setText = document.querySelector("h1").innerHTML
 if (randomNumber1 > randomNumber2){
-  document.querySelector("h1").innerHTML = "✨ Player 1 Wins! ✨ "
+  document.querySelector("h1").innerHTML = "Player 1 Wins!"
 } else if (randomNumber1 < randomNumber2){
-  document.querySelector("h1").innerHTML = "✨ Player 2 Wins! ✨ "
+  document.querySelector("h1").innerHTML = "Player 2 Wins!"
 } else {
-  document.querySelector("h1").innerHTML = "🚩 Draw"
+  document.querySelector("h1").innerHTML = "Draw"
 }
